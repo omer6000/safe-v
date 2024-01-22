db1b552f4d19c73d62af4c9be949ae1d64d40c38
omer6000 <63969246+omer6000@users.noreply.github.com>
2024-01-22 14:28:46 +0100

    Add files via upload

---
+++ src/fault_tree.rs
@@ -0,0 +1,29 @@
+use super::*;
+
+pub(super) fn minimal_cut_sets(tree: &Tree) -> BTreeSet<BTreeSet<Event>> {
+    todo!()
+}
+
+pub(super) fn path_set(tree: &Tree, events: &BTreeSet<Event>) -> bool {
+    todo!()
+}
+
+pub(super) fn minimal_path_sets(tree: &Tree) -> BTreeSet<BTreeSet<Event>> {
+    todo!()
+}
+
+pub(super) fn failure_probability(tree: &Tree) -> Ratio {
+    todo!()
+}
+
+pub(super) fn q0(tree: &Tree, override_events: &BTreeSet<Event>) -> Ratio {
+    todo!()
+}
+
+pub(super) fn birnbaum_importance(tree: &Tree, event: &str) -> Ratio {
+    todo!()
+}
+
+pub(super) fn improvement_potential_importance(tree: &Tree, event: &str) -> Ratio {
+    todo!()
+}
