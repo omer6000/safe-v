36d843912a044aba244986e55927d19802f4d2ae
omer6000 <omeriqbal7@hotmail.com>
2024-01-24 02:03:05 +0100

    min cut sets

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This is a project of the "Hands-On Dependability" course.
 For more details, check out the [task sheet](https://hod.cs.uni-saarland.de/projects/P03.html).
+
+docker run --rm --cpus=1 --stop-timeout 180 -v $PWD/src/fault_tree.rs:/opt/safe-v/src/fault_tree.rs -w /opt/safe-v hod cargo test -v
+cargo run --release --bin bench
--- src/fault_tree.rs
+++ src/fault_tree.rs
@@ -1,7 +1,46 @@
 use super::*;
 
 pub(super) fn minimal_cut_sets(tree: &Tree) -> BTreeSet<BTreeSet<Event>> {
-    todo!()
+    match tree {
+        Tree::BasicEvent(e) => {
+            let event_set: BTreeSet<Event> = BTreeSet::from_iter(vec![e.clone()]);
+            let cut_sets: BTreeSet<BTreeSet<Event>> = BTreeSet::from_iter(vec![event_set]);
+            return cut_sets
+        },
+        Tree::Gate(gate) => {
+            match gate {
+                Gate::Or(v) => {
+                    let min_sets : Vec<BTreeSet<BTreeSet<Event>>> = v.iter().map(|x| minimal_cut_sets(x)).collect();
+                    return  min_sets.into_iter().flat_map(|s| s).collect();
+                }
+                Gate::And(v) => {
+                    let mut result = BTreeSet::new();
+                    let set_vec : Vec<BTreeSet<BTreeSet<Event>>> = v.iter().map(|x| minimal_cut_sets(x)).collect();
+                    let vec_size= set_vec.len();
+                    for i in 0..vec_size {
+                        let res_size = result.len();
+                        if res_size == 0 {
+                            result = set_vec[i].clone();
+                        } else {
+                            let temp_set = result.clone();
+                            result.clear();
+                            for elem1 in &set_vec[i] {
+                                for elem2 in &temp_set {
+                                    let mut comb_elem = BTreeSet::new();
+                                    comb_elem.extend(elem1.clone());
+                                    comb_elem.extend(elem2.clone());
+                                    result.insert(comb_elem);
+                                }
+                            }
+                        }
+                    }
+                    return result
+                }
+            }
+        },
+        Tree::IntermediateEvent(_,t) => minimal_cut_sets(t)
+    }
+
 }
 
 pub(super) fn path_set(tree: &Tree, events: &BTreeSet<Event>) -> bool {
